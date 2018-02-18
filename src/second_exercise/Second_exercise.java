a9a89f6b60988230d55d6137af0bfd11778e9354
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-18 21:08:01 +0200

    Manually implementation of second exercise:

--- src/second_exercise/Second_exercise.java
+++ src/second_exercise/Second_exercise.java
@@ -6,9 +6,22 @@ public class Second_exercise {
         String var = "Hello Doston Hamrakulov";
         System.out.println(var);
         System.out.println(UsingLibrary(var));
+        System.out.println(Manually(var));
     }
     public static String UsingLibrary(String name){
         String newName = new StringBuilder(name).reverse().toString();
         return newName;
+    }
+    public static String Manually(String name){
+
+        char dos[] = new char[name.length()];
+        for (int i = 1; i <= name.length(); i++){
+            if (i > name.length()){
+                break;
+            } else {
+                dos[i-1] = name.charAt(name.length()-i);
+            }
+        }
+        return new String(dos) + "";
     }
 }
