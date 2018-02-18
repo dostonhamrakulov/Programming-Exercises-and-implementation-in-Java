1d11532da531ad18751b931aaed8a1fcff53f196
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-18 20:33:17 +0200

    First exercise

--- src/First_exercise/First_exercise.java
+++ src/First_exercise/First_exercise.java
@@ -11,6 +11,8 @@ public class First_exercise {
         number_1 = number_1 - number_2;
 
         System.out.println("First: " + number_1);
-        System.out.println("\nSecond: " + number_2);
+        System.out.println("Second: " + number_2);
+
+        System.out.println("Doston Hamrakulov doston.hamrakulov@gmail.com");
     }
 }
